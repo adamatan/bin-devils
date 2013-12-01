d4b498bae165e2a06beb4fba3cc24c46a2ba5ff2
Adam Matan <adam@matan.name>
2013-12-01 10:09:23 +0200

    Update jartree.sh

--- jartree.sh
+++ jartree.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Print the classes and methods in a jar file.
+# Credit: http://stackoverflow.com/questions/3248335/how-to-view-the-method-members-of-a-class-within-a-jar-file-thru-the-command-lin
+
 # Set the JAR name
 JAR=$1
 
