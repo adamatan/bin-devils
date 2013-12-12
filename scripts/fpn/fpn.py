8e246fa19e2703789d53cc22f4d9c8d59d7e02ee
Adam Matan <adam@matan.name>
2013-12-12 12:40:25 +0200

    fpn defaults to * if no filenames are set

--- scripts/fpn/fpn.py
+++ scripts/fpn/fpn.py
@@ -10,15 +10,14 @@ import socket
 ############################### Parse arguments ################################
 parser    = argparse.ArgumentParser(description='Display the full path name of a file.')
 
-parser.add_argument('filename', type=str, nargs='+',
-					help='one or more filenames or directory names.')
+parser.add_argument('filename', type=str, nargs='*', default=[f for f in os.listdir(os.getcwdu())],
+					help="One or more filenames or directory names. Defaults to all files in the current working directory, like 'ls -a'.")
 parser.add_argument('-f', '--files-only', help="List only files, not directories", default=False, action='store_true')
 parser.add_argument('-r', '--rsync', help="""Prefix "whoami@hostname:" to the full path for rsync-ing from a remote computer.""", default=False, action='store_true')
 
 args = parser.parse_args()
 
 rsync_prefix = "%s@%s:" % (getpass.getuser(), socket.gethostname()) if args.rsync else ""
-
 
 for f in args.filename:
     f=f.decode('utf8')
